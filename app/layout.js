ee183b76a6ff6d4f3213d853694cc55994946d7e
typicalryanb3-sys <typicalryanb3@gmail.com>
2026-02-27 22:56:10 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,15 +8,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body style={{ margin: 0 }}>
-  <nav style={{
-    padding: 15,
-    borderBottom: "2px solid black",
-    marginBottom: 20
-  }}>
-    <Link href="/" style={{ marginRight: 20 }}>Home</Link>
-    <Link href="/roster" style={{ marginRight: 20 }}>Roster</Link>
-    <Link href="/summary">Summary</Link>
-  </nav>
+<nav style={{
+  padding: 15,
+  borderBottom: "2px solid black",
+  marginBottom: 20
+}}>
+  <Link href="/" style={{ marginRight: 20 }}>Home</Link>
+  <Link href="/roster" style={{ marginRight: 20 }}>Roster</Link>
+  <Link href="/summary" style={{ marginRight: 20 }}>Game Summary</Link>
+  <Link href="/schedule">Game Schedule</Link>
+</nav>
 
         {children}
       </body>
