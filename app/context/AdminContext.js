0cfce8bd3d2e0437119fd8dfb4d241a2429a5b63
typicalryanb3-sys <typicalryanb3@gmail.com>
2026-02-28 00:22:15 -0600

    Create AdminContext.js

---
+++ app/context/AdminContext.js
@@ -0,0 +1,31 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+const AdminContext = createContext();
+
+export function AdminProvider({ children }) {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const login = (password) => {
+    if (password === "coach123") {
+      setIsAdmin(true);
+    } else {
+      alert("Wrong password");
+    }
+  };
+
+  const logout = () => {
+    setIsAdmin(false);
+  };
+
+  return (
+    <AdminContext.Provider value={{ isAdmin, login, logout }}>
+      {children}
+    </AdminContext.Provider>
+  );
+}
+
+export function useAdmin() {
+  return useContext(AdminContext);
+}
