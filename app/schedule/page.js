bd9d302fdbdd7afaf4f8b1bfe0ce769382fecad5
typicalryanb3-sys <typicalryanb3@gmail.com>
2026-02-27 23:02:52 -0600

    Create page.js

---
+++ app/schedule/page.js
@@ -0,0 +1,15 @@
+export default function SchedulePage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Game Schedule</h1>
+
+      <p>Add upcoming games here.</p>
+
+      <ul>
+        <li>Game 1 - TBD</li>
+        <li>Game 2 - TBD</li>
+        <li>Game 3 - TBD</li>
+      </ul>
+    </div>
+  );
+}
