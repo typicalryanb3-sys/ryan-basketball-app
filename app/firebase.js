06f7c326d9388967d27f7d2668b7f4dd5c8db434
typicalryanb3-sys <typicalryanb3@gmail.com>
2026-03-24 18:47:31 -0500

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBvM43PBZKR_CC0sF_PahmeoCUA_Y8endQ",
   authDomain: "ryan-basketball-app.firebaseapp.com",
   projectId: "ryan-basketball-app",
-  storageBucket: "ryan-basketball-app.firebasestorage.app",
+  storageBucket: "ryan-basketball-app.appspot.com",
   messagingSenderId: "57766695790",
   appId: "1:57766695790:web:1093ea82c5554af081677f"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
